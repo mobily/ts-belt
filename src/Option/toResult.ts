15709204cfeab9e62bb7d60e49cd2713ab3fcd55
Marcin Dziewulski <hello@mobily.pl>
2020-12-02 20:06:38 +0100

    Update toResult types

--- src/Option/toResult.ts
+++ src/Option/toResult.ts
@@ -4,10 +4,12 @@ import { curry2 } from '../utils'
 import { isNone } from './isNone'
 
 type Curry2 = {
-  <A, B>(error: B): (option: Type.Option<A>) => Type.Result<A, B>
-  <A, B>(error: B, option: Type.Option<A>): Type.Result<A, B>
+  <A, B>(error: NonNullable<B>): (option: Type.Option<A>) => Type.Result<A, B>
+  <A, B>(error: NonNullable<B>, option: Type.Option<A>): Type.Result<A, B>
 }
 
-export const toResult: Curry2 = curry2(<A, B>(error: B, option: Type.Option<A>): any => {
-  return isNone(option) ? Error(error) : Ok(option.value)
-})
+export const toResult: Curry2 = curry2(
+  <A, B>(error: NonNullable<B>, option: Type.Option<A>): any => {
+    return isNone(option) ? Error(error) : Ok(option.value)
+  },
+)
