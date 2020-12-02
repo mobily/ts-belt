e22217df2fc8b6d7a890823d3514c6f9755b8669
Marcin Dziewulski <hello@mobily.pl>
2020-12-02 19:57:09 +0100

    Update types definitions

--- src/types.ts
+++ src/types.ts
@@ -3,10 +3,10 @@ type Constructor<T> = {
   readonly __type: symbol
 }
 
-export type Some<T> = Constructor<T> & { __: 'Some' }
+export type Some<T> = Constructor<NonNullable<T>> & { __: 'Some' }
 export type None = Constructor<never> & { __: 'None' }
-export type Ok<T> = Constructor<T> & { __: 'Ok' }
-export type Error<T> = Constructor<T> & { __: 'Error' }
+export type Ok<T> = Constructor<NonNullable<T>> & { __: 'Ok' }
+export type Error<T> = Constructor<NonNullable<T>> & { __: 'Error' }
 
 export type Option<T> = Some<T> | None
 export type Result<A, B> = Ok<A> | Error<B>
