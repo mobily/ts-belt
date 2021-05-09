41beb9825cb54b3a00d32f53201fb188f0755988
Marcin Dziewulski <hello@mobily.pl>
2021-05-09 20:19:39 +0200

    Implement Result helpers

---
+++ src/Result.res
@@ -0,0 +1,29 @@
+let fromNullable = (error, value) =>
+  Js.Nullable.isNullable(value) ? Belt.Result.Error(error) : Belt.Result.Ok(value)
+let fromFalsy = (error, value) => !value ? Error(error) : Ok(value)
+let fromPredicate = (predicateFn, error, value) =>
+  predicateFn(value) ? fromNullable(error, value) : Error(error)
+
+let map = (mapFn, result) => Belt.Result.map(result, mapFn)
+let mapWithDefault = (defaultValue, mapFn, result) =>
+  Belt.Result.mapWithDefault(result, defaultValue, mapFn)
+let flatMap = (mapFn, result) => Belt.Result.flatMap(result, mapFn)
+
+let getExn = Belt.Result.getExn
+let getWithDefault = (defaultValue, result) => Belt.Result.getWithDefault(result, defaultValue)
+
+let toUndefined = result => Belt.Result.getWithDefault(result, Js.undefined)
+let toNullable = result => Belt.Result.getWithDefault(result, Js.null)
+let toOption = result =>
+  switch result {
+  | Ok(value) => Some(value)
+  | Error(_) => None
+  }
+let match = (okFn, errorFn, result) =>
+  switch result {
+  | Ok(value) => okFn(value)
+  | Error(value) => errorFn(value)
+  }
+
+let isError = Belt.Result.isError
+let isOk = Belt.Result.isOk
---
+++ src/Result.ts
@@ -0,0 +1,63 @@
+import type { Option } from './Option'
+import type { MapFn, PredicateFn } from './types'
+
+export declare type Ok<T> = {
+  readonly TAG: 0
+  readonly _0: NonNullable<T>
+} & { __: 'Ok' }
+export declare type Error<T> = {
+  readonly TAG: 1
+  readonly _0: NonNullable<T>
+} & { __: 'Error' }
+
+export declare type Result<A, B> = Ok<A> | Error<B>
+
+export declare const Ok: <T>(value: NonNullable<T>) => Ok<T>
+export declare const Error: <T>(value: NonNullable<T>) => Error<T>
+
+export declare const fromNullable: {
+  <B>(error: NonNullable<B>): <A>(value: A) => Result<A, B>
+  <A, B>(error: NonNullable<B>, value: A): Result<A, B>
+}
+export declare const fromFalsy: {
+  <B>(error: NonNullable<B>): (value: 0 | '' | false | null | undefined) => Error<B>
+  <B>(error: NonNullable<B>, value: 0 | '' | false | null | undefined): Error<B>
+  <B>(error: NonNullable<B>): <A>(value: A) => Result<A, B>
+  <A, B>(error: NonNullable<B>, value: A): Result<A, B>
+}
+export declare const fromPredicate: {
+  <A>(predicateFn: PredicateFn<[A]>): <B>(error: NonNullable<B>) => <A>(value: A) => Result<A, B>
+  <A, B>(predicateFn: PredicateFn<[A]>, error: NonNullable<B>): (value: A) => Result<A, B>
+  <A, B>(predicateFn: PredicateFn<[A]>, error: NonNullable<B>, value: A): Result<A, B>
+}
+export declare const flatMap: {
+  <A, B, R>(mapFn: MapFn<A, Result<R, B>>): (result: Result<A, B>) => Result<R, B>
+  <A, B, R>(mapFn: MapFn<A, Result<R, B>>, result: Result<A, B>): Result<R, B>
+}
+export declare const map: {
+  <A, B, R>(fn: MapFn<A, NonNullable<R>>): (result: Result<A, B>) => Result<R, B>
+  <A, B, R>(fn: MapFn<A, NonNullable<R>>, result: Result<A, B>): Result<R, B>
+}
+export declare const mapWithDefault: {
+  <A, B, R>(defaultValue: NonNullable<R>): (
+    fn: MapFn<A, NonNullable<R>>,
+  ) => (result: Result<A, B>) => R
+  <A, B, R>(defaultValue: NonNullable<R>, fn: MapFn<A, NonNullable<R>>): (result: Result<A, B>) => R
+  <A, B, R>(defaultValue: NonNullable<R>, fn: MapFn<A, NonNullable<R>>, result: Result<A, B>): R
+}
+export declare const getWithDefault: {
+  <A>(defaultValue: NonNullable<A>): <B>(result: Result<A, B>) => A
+  <A, B>(defaultValue: NonNullable<A>, result: Result<A, B>): A
+}
+export declare const getExn: <A, B>(result: Result<A, B>) => A | never
+export declare const match: {
+  <A, R>(okFn: (value: A) => R): <B>(errorFn: (value: B) => R) => (result: Result<A, B>) => R
+  <A, B, R>(okFn: (value: A) => R, errorFn: (value: B) => R): (result: Result<A, B>) => R
+  <A, B, R>(okFn: (value: A) => R, errorFn: (value: B) => R, result: Result<A, B>): R
+}
+export declare const toNullable: <A, B>(result: Result<A, B>) => A | null
+export declare const toOption: <A, B>(result: Result<A, B>) => Option<A>
+export declare const toUndefined: <A, B>(result: Result<A, B>) => A | undefined
+
+export declare const isOk: <A, B>(result: Result<A, B>) => result is Ok<A>
+export declare const isError: <A, B>(result: Result<A, B>) => result is Error<B>
