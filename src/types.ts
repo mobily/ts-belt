c78540d1fe67b75e38c57c3e5cd1b35132c8fbc1
Marcin Dziewulski <hello@mobily.pl>
2021-05-16 14:27:44 +0200

    Update MapFn type

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
 export declare type PredicateFn<T extends any[]> = (...args: T) => boolean
-export declare type MapFn<T, R> = (value: T) => R
+export declare type MapFn<T extends any[], R> = (...args: T) => R
 
 export declare type UnaryFunction<T, R> = (value: T) => R
 
