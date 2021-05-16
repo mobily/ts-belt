67f78ade4f80b32e566cab734e53b3ee6dda6ef0
Marcin Dziewulski <hello@mobily.pl>
2021-05-16 14:28:08 +0200

    Update Array helpers

--- src/Array.res
+++ src/Array.res
@@ -1,20 +1,16 @@
 let length = Belt.Array.length
-let drop = (i, xs) => {
-  let l = length(xs)
-  let start = i < 0 ? 0 : l < i ? l : i
-  Belt.Array.sliceToEnd(xs, start)
-}
-let get = (i, xs) => xs->Belt.Array.get(i)->Belt.Option.map(Option.fromNullable)
+let isEmpty = xs => length(xs) == 0
+let isNotEmpty = xs => length(xs) > 0
+let reverse = Belt.Array.reverse
+let prepend = (el, xs) => Belt.Array.concat([el], xs)
+let append = (el, xs) => Belt.Array.concat(xs, [el])
+let get = (i, xs) => xs->Belt.Array.get(i)->Belt.Option.flatMap(Option.fromNullable)
 let getBy = (predicateFn, xs) =>
-  xs->Belt.Array.getBy(predicateFn)->Belt.Option.map(Option.fromNullable)
+  xs->Belt.Array.getBy(predicateFn)->Belt.Option.flatMap(Option.fromNullable)
 let head = xs => get(0, xs)
-let splitAt = (i, xs) => {
-  if i < 0 || i > length(xs) {
-    None
-  } else {
-    Some((Belt.Array.slice(xs, ~offset=0, ~len=i), Belt.Array.sliceToEnd(xs, i)))
-  }
-}
+
+let at = (i, xs) => Belt.Array.get(xs, i)
+
 let tail = xs => {
   let l = length(xs)
 
@@ -23,12 +19,128 @@ let tail = xs => {
   } else if l == 1 {
     Some([])
   } else {
-    let ys = Belt.Array.sliceToEnd(xs, 1)
-    length(ys) > 0 ? Some(ys) : None
+    let xs = Belt.Array.sliceToEnd(xs, 1)
+    isNotEmpty(xs) ? Some(xs) : None
   }
 }
+
+let tailOrEmpty = xs =>
+  switch tail(xs) {
+  | Some(xs) => xs
+  | None => []
+  }
+
 let take = (i, xs) => {
   let l = length(xs)
   let len = i < 0 ? 0 : l < i ? l : i
   Belt.Array.slice(xs, ~offset=0, ~len)
 }
+
+let takeExactly = (i, xs) =>
+  i < 0 || i > length(xs) ? None : Some(Belt.Array.slice(xs, ~offset=0, ~len=i))
+
+let rec takeWhile = (predicateFn, xs) =>
+  switch head(xs) {
+  | Some(el) if predicateFn(el) => prepend(el, takeWhile(predicateFn, tailOrEmpty(xs)))
+  | _ => []
+  }
+
+let drop = (i, xs) => {
+  let l = length(xs)
+  let start = i < 0 ? 0 : l < i ? l : i
+  Belt.Array.sliceToEnd(xs, start)
+}
+
+let dropExactly = (i, xs) => i < 0 || i > length(xs) ? None : Some(Belt.Array.sliceToEnd(xs, i))
+
+let rec dropWhile = (predicateFn, xs) =>
+  switch head(xs) {
+  | Some(el) if predicateFn(el) => dropWhile(predicateFn, tailOrEmpty(xs))
+  | _ => xs
+  }
+
+let uncons = xs =>
+  switch xs {
+  | [] => None
+  | _ => Some((Belt.Array.getExn(xs, 0), Belt.Array.sliceToEnd(xs, 1)))
+  }
+
+let splitAt = (i, xs) =>
+  i < 0 || i > length(xs)
+    ? None
+    : Some((Belt.Array.slice(xs, ~offset=0, ~len=i), Belt.Array.sliceToEnd(xs, i)))
+
+let rec splitEvery = (i, xs) =>
+  i < 1
+    ? [xs]
+    : Belt.Array.length(xs) <= i
+    ? [xs]
+    : xs |> drop(i) |> splitEvery(i) |> Belt.Array.concat([take(i, xs)])
+
+let shuffle = Belt.Array.shuffle
+let repeat = Belt.Array.make
+let makeWithIndex = Belt.Array.makeBy
+
+let map = (mapFn, xs) => Belt.Array.map(xs, mapFn)
+let mapWithIndex = (mapFn, xs) => Belt.Array.mapWithIndex(xs, (i, el) => mapFn(el, i))
+
+let filter = (fn, xs) => Belt.Array.keep(xs, fn)
+let filterWithIndex = (fn, xs) => Belt.Array.keepWithIndex(xs, fn)
+
+let reject = (fn, xs) => filter(el => !fn(el), xs)
+let rejectWithIndex = (fn, xs) => filterWithIndex((el, i) => !fn(el, i), xs)
+
+let init = xs => {
+  let l = length(xs)
+  l == 0 ? None : Some(Belt.Array.slice(xs, ~offset=0, ~len=l - 1))
+}
+
+let initOrEmpty = xs =>
+  switch init(xs) {
+  | Some(xs) => xs
+  | None => []
+  }
+
+let last = xs => {
+  let l = length(xs)
+  l == 0 ? None : get(l - 1, xs)
+}
+
+let partition = (fn, xs) => Belt.Array.partition(xs, fn)
+let concat = (xs, ys) => Belt.Array.concat(ys, xs)
+let concatMany = Belt.Array.concatMany
+let every = (fn, xs) => Belt.Array.every(xs, fn)
+let some = (fn, xs) => Belt.Array.some(xs, fn)
+let slice = (offset, len, xs) => Belt.Array.slice(xs, ~offset, ~len)
+let sliceToEnd = (offset, xs) => Belt.Array.sliceToEnd(xs, offset)
+let eq = (cmp, xs, ys) => Belt.Array.eq(xs, ys, cmp)
+let range = Belt.Array.range
+let rangeBy = (x, y, step) => Belt.Array.rangeBy(x, y, ~step)
+let reduce = (fn, acc, xs) => Belt.Array.reduce(xs, acc, fn)
+let reduceWithIndex = (fn, acc, xs) => Belt.Array.reduceWithIndex(xs, acc, fn)
+let copy = Belt.Array.copy
+let zip = (xs, ys) => Belt.Array.zip(ys, xs)
+let zipWith = (fn, xs, ys) => Belt.Array.zipBy(ys, xs, fn)
+let unzip = Belt.Array.unzip
+let replaceAt = (targetIndex, el, xs) =>
+  xs |> mapWithIndex((current, currentIndex) => currentIndex == targetIndex ? el : current)
+let insertAt = (targetIndex, el, xs) =>
+  switch splitAt(targetIndex, xs) {
+  | Some((before, after)) => concat(before, concat([el], after))
+  | None => xs
+  }
+let updateAt = (targetIndex, fn, xs) =>
+  xs |> mapWithIndex((x, index) => index == targetIndex ? fn(x) : x)
+let swapAt = (targetIndex, swapIndex, xs) =>
+  switch (at(targetIndex, xs), at(swapIndex, xs)) {
+  | (Some(a), Some(b)) =>
+    xs |> mapWithIndex((x, k) => targetIndex == k ? b : swapIndex == k ? a : x)
+  | _ => xs
+  }
+let removeAt = (targetIndex, xs) => xs |> filterWithIndex((_, i) => i != targetIndex)
+
+let uniqBy = (predicateFn, xs) =>
+  Belt.Array.reduce(xs, (acc, value) =>
+    Belt.Array.some(acc, el => el == predicateFn(value)) ? acc : append(value, acc)
+  )
+let uniq = xs => uniqBy(el => el, xs)
--- src/Array.ts
+++ src/Array.ts
@@ -1,28 +1,173 @@
 import type { Option } from './Option'
-import type { PredicateFn } from './types'
+import type { PredicateFn, MapFn } from './types'
 
-declare type TupleOf<T> = readonly [T, T]
+declare type Array<T> = readonly T[]
 
+export declare const length: <T>(xs: Array<T>) => number
+export declare const isEmpty: <T>(xs: Array<T>) => boolean
+export declare const isNotEmpty: <T>(xs: Array<T>) => boolean
+export declare const reverse: <T>(xs: Array<T>) => Array<T>
+export declare const prepend: {
+  <T>(el: T): (xs: Array<T>) => Array<T>
+  <T>(el: T, xs: Array<T>): Array<T>
+}
+export declare const append: {
+  <T>(el: T): (xs: Array<T>) => Array<T>
+  <T>(el: T, xs: Array<T>): Array<T>
+}
 export declare const get: {
-  (i: number): <T>(xs: readonly T[]) => Option<T>
-  <T>(i: number, xs: readonly T[]): Option<T>
+  (i: number): <T>(xs: Array<T>) => Option<T>
+  <T>(i: number, xs: Array<T>): Option<T>
 }
 export declare const getBy: {
-  <T>(predicateFn: PredicateFn<[T, number]>): (xs: readonly T[]) => Option<T>
-  <T>(predicateFn: PredicateFn<[T, number]>, xs: readonly T[]): Option<T>
+  <T>(predicateFn: PredicateFn<[T, number]>): (xs: Array<T>) => Option<T>
+  <T>(predicateFn: PredicateFn<[T, number]>, xs: Array<T>): Option<T>
+}
+export declare const head: <T>(xs: Array<T>) => Option<T>
+export declare const tail: <T>(xs: Array<T>) => Option<Array<T>>
+export declare const tailOrEmpty: <T>(xs: Array<T>) => Array<T>
+export declare const take: {
+  (i: number): <T>(xs: Array<T>) => Array<T>
+  <T>(i: number, xs: Array<T>): Array<T>
+}
+export declare const takeExactly: {
+  (i: number): <T>(xs: Array<T>) => Option<Array<T>>
+  <T>(i: number, xs: Array<T>): Option<Array<T>>
+}
+export declare const takeWhile: {
+  <T>(predicateFn: PredicateFn<[T]>): (xs: Array<T>) => Array<T>
+  <T>(predicateFn: PredicateFn<[T]>, xs: Array<T>): Array<T>
 }
 export declare const drop: {
-  (i: number): <T>(xs: readonly T[]) => readonly T[]
-  <T>(i: number, xs: readonly T[]): readonly T[]
+  (i: number): <T>(xs: Array<T>) => Array<T>
+  <T>(i: number, xs: Array<T>): Array<T>
 }
-export declare const head: <T>(xs: readonly T[]) => Option<T>
+export declare const dropExactly: {
+  (i: number): <T>(xs: Array<T>) => Option<Array<T>>
+  <T>(i: number, xs: Array<T>): Option<Array<T>>
+}
+export declare const dropWhile: {
+  <T>(predicateFn: PredicateFn<[T]>): (xs: Array<T>) => Array<T>
+  <T>(predicateFn: PredicateFn<[T]>, xs: Array<T>): Array<T>
+}
+export declare const uncons: <T>(xs: Array<T>) => Option<readonly [T, Array<T>]>
 export declare const splitAt: {
-  (i: number): <T>(xs: readonly T[]) => Option<TupleOf<readonly T[]>>
-  <T>(i: number, xs: readonly T[]): Option<TupleOf<readonly T[]>>
+  (i: number): <T>(xs: Array<T>) => Option<readonly [Array<T>, Array<T>]>
+  <T>(i: number, xs: Array<T>): Option<readonly [Array<T>, Array<T>]>
 }
-export declare const tail: <T>(xs: readonly T[]) => Option<readonly T[]>
-export declare const take: {
-  <T>(i: number): (xs: readonly T[]) => readonly T[]
-  <T>(i: number, xs: readonly T[]): readonly T[]
+export declare const splitEvery: {
+  (i: number): <T>(xs: Array<T>) => Array<Array<T>>
+  <T>(i: number, xs: Array<T>): Array<Array<T>>
 }
-export declare const length: <T>(xs: readonly T[]) => number
+export declare const shuffle: <T>(xs: Array<T>) => Array<T>
+export declare const repeat: <T>(i: number, el: T) => Array<T>
+export declare const makeWithIndex: <T>(i: number, mapFn: MapFn<[number], T>) => Array<T>
+export declare const map: {
+  <T, R>(mapFn: MapFn<[T], R>): (xs: Array<T>) => Array<R>
+  <T, R>(mapFn: MapFn<[T], R>, xs: Array<T>): Array<R>
+}
+export declare const mapWithIndex: {
+  <T, R>(mapFn: MapFn<[T, number], R>): (xs: Array<T>) => Array<R>
+  <T, R>(mapFn: MapFn<[T, number], R>, xs: Array<T>): Array<R>
+}
+export declare const filter: {
+  <T, R>(predicateFn: PredicateFn<[T]>): (xs: Array<T>) => Array<R>
+  <T, R>(predicateFn: PredicateFn<[T]>, xs: Array<T>): Array<R>
+}
+export declare const filterWithIndex: {
+  <T, R>(predicateFn: PredicateFn<[T, number]>): (xs: Array<T>) => Array<R>
+  <T, R>(predicateFn: PredicateFn<[T, number]>, xs: Array<T>): Array<R>
+}
+export declare const reject: {
+  <T, R>(predicateFn: PredicateFn<[T]>): (xs: Array<T>) => Array<R>
+  <T, R>(predicateFn: PredicateFn<[T]>, xs: Array<T>): Array<R>
+}
+export declare const rejectWithIndex: {
+  <T, R>(predicateFn: PredicateFn<[T, number]>): (xs: Array<T>) => Array<R>
+  <T, R>(predicateFn: PredicateFn<[T, number]>, xs: Array<T>): Array<R>
+}
+export declare const init: <T>(xs: Array<T>) => Option<Array<T>>
+export declare const initOrEmpty: <T>(xs: Array<T>) => Array<T>
+export declare const last: <T>(xs: Array<T>) => Option<T>
+export declare const partition: {
+  <T>(predicateFn: PredicateFn<[T]>): (xs: Array<T>) => readonly [Array<T>, Array<T>]
+  <T>(predicateFn: PredicateFn<[T]>, xs: Array<T>): readonly [Array<T>, Array<T>]
+}
+export declare const concat: {
+  <T>(xs: Array<T>): <R>(ys: Array<R>) => Array<T & R>
+  <T, R>(xs: Array<T>, ys: Array<R>): Array<T & R>
+}
+export declare const concatMany: <T>(xs: Array<Array<T>>) => Array<T>
+export declare const every: {
+  <T>(predicateFn: PredicateFn<[T]>): (xs: Array<T>) => boolean
+  <T>(predicateFn: PredicateFn<[T]>, xs: Array<T>): boolean
+}
+export declare const some: {
+  <T>(predicateFn: PredicateFn<[T]>): (xs: Array<T>) => boolean
+  <T>(predicateFn: PredicateFn<[T]>, xs: Array<T>): boolean
+}
+export declare const slice: {
+  (offset: number): (len: number) => <T>(xs: Array<T>) => Array<T>
+  (offset: number, len: number): <T>(xs: Array<T>) => Array<T>
+  <T>(offset: number, len: number, xs: Array<T>): Array<T>
+}
+export declare const sliceToEnd: {
+  (offset: number): <T>(xs: Array<T>) => Array<T>
+  <T>(offset: number, xs: Array<T>): Array<T>
+}
+export declare const eq: {
+  <T, R>(predicateFn: PredicateFn<[T, R]>): (xs: Array<T>) => (ys: Array<R>) => boolean
+  <T, R>(predicateFn: PredicateFn<[T, R]>, xs: Array<T>): (ys: Array<R>) => boolean
+  <T, R>(predicateFn: PredicateFn<[T, R]>, xs: Array<T>, ys: Array<R>): boolean
+}
+export declare const range: (start: number, finish: number) => Array<number>
+export declare const rangeBy: {
+  (start: number): (finish: number) => (step: number) => Array<number>
+  (start: number, finish: number): (step: number) => Array<number>
+  (start: number, finish: number, step: number): Array<number>
+}
+export declare const reduce: {
+  <T, A>(mapFn: MapFn<[A, T], A>): (acc: A) => (xs: Array<T>) => A
+  <T, A>(mapFn: MapFn<[A, T], A>, acc: A): (xs: Array<T>) => A
+  <T, A>(mapFn: MapFn<[A, T], A>, acc: A, xs: Array<T>): A
+}
+export declare const reduceWithIndex: {
+  <T, A>(mapFn: MapFn<[A, T, number], A>): (acc: A) => (xs: Array<T>) => A
+  <T, A>(mapFn: MapFn<[A, T, number], A>, acc: A): (xs: Array<T>) => A
+  <T, A>(mapFn: MapFn<[A, T, number], A>, acc: A, xs: Array<T>): A
+}
+export declare const copy: <T>(xs: Array<T>) => Array<T>
+export declare const zip: {
+  <T>(xs: Array<T>): <R>(ys: Array<R>) => Array<readonly [R, T]>
+  <T, R>(xs: Array<T>, ys: Array<R>): Array<readonly [R, T]>
+}
+export declare const zipWith: {
+  <B, A, R>(mapFn: MapFn<[A, B], R>): (ys: Array<B>) => (xs: Array<A>) => Array<R>
+  <B, A, R>(mapFn: MapFn<[A, B], R>, ys: Array<B>): (xs: Array<A>) => Array<R>
+  <B, A, R>(mapFn: MapFn<[A, B], R>, ys: Array<B>, xs: Array<A>): Array<R>
+}
+export declare const unzip: <A, B>(xs: Array<readonly [A, B]>) => readonly [Array<A>, Array<B>]
+export declare const replaceAt: {
+  (targetIndex: number): <T>(el: T) => (xs: Array<T>) => Array<T>
+  <T>(targetIndex: number, el: T): (xs: Array<T>) => Array<T>
+  <T>(targetIndex: number, el: T, xs: Array<T>): Array<T>
+}
+export declare const insertAt: {
+  (targetIndex: number): <T>(el: T) => (xs: Array<T>) => Array<T>
+  <T>(targetIndex: number, el: T): (xs: Array<T>) => Array<T>
+  <T>(targetIndex: number, el: T, xs: Array<T>): Array<T>
+}
+export declare const updateAt: {
+  (targetIndex: number): <T>(mapFn: MapFn<[T], T>) => (xs: Array<T>) => Array<T>
+  <T>(targetIndex: number, mapFn: MapFn<[T], T>): (xs: Array<T>) => Array<T>
+  <T>(targetIndex: number, mapFn: MapFn<[T], T>, xs: Array<T>): Array<T>
+}
+export declare const swapAt: {
+  (targetIndex: number): (swapIndex: number) => <T>(xs: Array<T>) => Array<T>
+  (targetIndex: number, swapIndex: number): <T>(xs: Array<T>) => Array<T>
+  <T>(targetIndex: number, swapIndex: number, xs: Array<T>): Array<T>
+}
+export declare const removeAt: {
+  (targetIndex: number): <T>(xs: Array<T>) => Array<T>
+  <T>(targetIndex: number, xs: Array<T>): Array<T>
+}
