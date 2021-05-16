5fd4089ac18430a3f3ea48f898eff1a866afde08
Marcin Dziewulski <hello@mobily.pl>
2021-05-16 20:02:58 +0200

    Update benchmark suites

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,7 +1,8 @@
-const { A, pipe } = require('..')
+const { A, F, pipe } = require('..')
 
 const Ramda = require('ramda')
 const Remeda = require('remeda')
+const lodash = require('lodash/fp')
 
 const input = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 const input2 = [1, 2, 2, 3, 3, 5, 4, 3, 2, 1]
@@ -32,6 +33,14 @@ suite('[1, 2, 3, 4, 5, 6, 7, 8, 9] → map → filter → reduce', () => {
       Remeda.reduce((acc, x) => acc + x, 0),
     )
   })
+
+  benchmark('lodash', () => {
+    return lodash.pipe(
+      lodash.map(x => x * 2),
+      lodash.filter(x => x > 4),
+      lodash.reduce((acc, x) => acc + x, 0),
+    )(input)
+  })
 })
 
 suite('[1, 2, 2, 3, 3, 5, 4, 3, 2, 1] → uniq → take', () => {
@@ -45,5 +54,27 @@ suite('[1, 2, 2, 3, 3, 5, 4, 3, 2, 1] → uniq → take', () => {
 
   benchmark('remeda', () => {
     return Remeda.pipe(input2, Remeda.uniq(), Remeda.take(3))
+  })
+
+  benchmark('lodash', () => {
+    return lodash.pipe(lodash.uniq, lodash.take(3))(input2)
+  })
+})
+
+suite('equals [1, 2, 3, 4, 5] [2, 4, 5, 1, 9]', () => {
+  benchmark('@mobily/ts-belt', () => {
+    return F.equals([1, 2, 3, 4, 5], [2, 4, 5, 1, 9])
+  })
+
+  benchmark('ramda', () => {
+    return Ramda.equals([1, 2, 3, 4, 5], [2, 4, 5, 1, 9])
+  })
+
+  benchmark('remeda', () => {
+    return Remeda.equals([1, 2, 3, 4, 5], [2, 4, 5, 1, 9])
+  })
+
+  benchmark('lodash', () => {
+    return lodash.equals([1, 2, 3, 4, 5], [2, 4, 5, 1, 9])
   })
 })
