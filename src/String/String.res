e5181211cef0861c9338842088292ebaf8e44c1d
Marcin Dziewulski <hello@mobily.pl>
2021-05-29 16:38:17 +0200

    Add String helpers

--- src/String/String.res
+++ src/String/String.res
@@ -1,6 +1,8 @@
+let length = Js.String2.length
 let head = str => Js.String2.slice(str, ~from=0, ~to_=1)
 let slice = (from, to_, str) => Js.String2.slice(str, ~from, ~to_)
 let sliceToEnd = (from, str) => Js.String2.sliceToEnd(str, ~from)
 let toLowerCase = Js.String2.toLowerCase
 let toUpperCase = Js.String2.toUpperCase
 let trim = Js.String2.trim
+let isEmpty = str => length(str) == 0
--- src/String/index.ts
+++ src/String/index.ts
@@ -1,3 +1,4 @@
+export declare const length: (str: string) => number
 export declare const head: (str: string) => string
 export declare const slice: {
   (from: number): (to: number) => (xs: string) => string
@@ -11,3 +12,4 @@ export declare const sliceToEnd: {
 export declare const toLowerCase: (str: string) => string
 export declare const toUpperCase: (str: string) => string
 export declare const trim: (str: string) => string
+export declare const isEmpty: (str: string) => boolean
