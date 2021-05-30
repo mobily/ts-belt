d60a473b17cf70e58d636b5e73a5c1549946e7e8
Marcin Dziewulski <hello@mobily.pl>
2021-05-30 10:55:33 +0200

    Add String helpers

--- src/String/String.res
+++ src/String/String.res
@@ -6,3 +6,7 @@ let toLowerCase = Js.String2.toLowerCase
 let toUpperCase = Js.String2.toUpperCase
 let trim = Js.String2.trim
 let isEmpty = str => length(str) == 0
+let split = (delimiter, str) => Js.String2.split(str, delimiter)
+let includes = (value, str) => Js.String2.includes(str, value)
+let replace = (oldValue, newValue, str) => Js.String2.replace(str, oldValue, newValue)
+let replaceByRe = (regex, value, str) => Js.String2.replaceByRe(str, regex, value)
--- src/String/index.ts
+++ src/String/index.ts
@@ -1,15 +1,35 @@
+import type { Array } from '../types'
+
 export declare const length: (str: string) => number
 export declare const head: (str: string) => string
 export declare const slice: {
-  (from: number): (to: number) => (xs: string) => string
-  (from: number, to: number): (xs: string) => string
-  (from: number, to: number, xs: string): string
+  (from: number): (to: number) => (str: string) => string
+  (from: number, to: number): (str: string) => string
+  (from: number, to: number, str: string): string
 }
 export declare const sliceToEnd: {
-  (from: number): (xs: string) => string
-  (from: number, xs: string): string
+  (from: number): (str: string) => string
+  (from: number, str: string): string
 }
 export declare const toLowerCase: (str: string) => string
 export declare const toUpperCase: (str: string) => string
 export declare const trim: (str: string) => string
 export declare const isEmpty: (str: string) => boolean
+export declare const split: {
+  (delimiter: string): (str: string) => Array<string>
+  (delimiter: string, str: string): Array<string>
+}
+export declare const includes: {
+  (value: string): (str: string) => boolean
+  (value: string, str: string): boolean
+}
+export declare const replace: {
+  (oldValue: string): (newValue: string) => (str: string) => string
+  (oldValue: string, newValue: string): (str: string) => string
+  (oldValue: string, newValue: string, str: string): string
+}
+export declare const replaceByRe: {
+  (regex: RegExp): (value: string) => (str: string) => string
+  (regex: RegExp, value: string): (str: string) => string
+  (regex: RegExp, value: string, str: string): string
+}
