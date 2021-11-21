bce676acc8af209ae2a4d72f0ec6efa23bf37ec4
Marcin Dziewulski <hello@mobily.pl>
2021-11-21 11:04:28 +0100

    Add forEach benchmark

---
+++ benchmark/simple/forEach.js
@@ -0,0 +1,73 @@
+const {
+  makeBenchmark,
+  addTsBelt,
+  addLodashFp,
+  addRambda,
+  addRamda,
+  addRemeda,
+} = require('../utils')
+
+const input = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const fn = x => x
+
+module.exports = makeBenchmark(
+  'forEach',
+  addTsBelt(tsBelt => {
+    const { A, pipe } = tsBelt
+
+    return [
+      () => {
+        return A.forEach(input, fn)
+      },
+      () => {
+        return pipe(input, A.forEach(fn))
+      },
+    ]
+  }),
+  addRemeda(remeda => {
+    const { pipe, forEach } = remeda
+
+    return [
+      () => {
+        return forEach(input, fn)
+      },
+      () => {
+        return pipe(input, forEach(fn))
+      },
+    ]
+  }),
+  addRamda(ramda => {
+    const { pipe, forEach } = ramda
+
+    return [
+      () => {
+        return forEach(fn, input)
+      },
+      () => {
+        return pipe(forEach(fn))(input)
+      },
+    ]
+  }),
+  addRambda(rambda => {
+    const { pipe, forEach } = rambda
+
+    return [
+      () => {
+        return forEach(fn, input)
+      },
+      () => {
+        return pipe(forEach(fn))(input)
+      },
+    ]
+  }),
+  addLodashFp(_ => {
+    return [
+      () => {
+        return _.forEach(fn, input)
+      },
+      () => {
+        return _.pipe(_.forEach(fn))(input)
+      },
+    ]
+  }),
+)
