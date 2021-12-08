85bd3cf577a9a6d5237ce1399778175a913b8634
Marcin Dziewulski <hello@mobily.pl>
2021-12-08 23:03:12 +0100

    Update tests

--- __tests__/String/head.test.ts
+++ __tests__/String/head.test.ts
@@ -1,15 +1,22 @@
+import { expectType } from 'ts-expect'
+
 import { S, O } from '../..'
 
- describe('head', () => {
-   it('returns None if the given string is empty.', () => {
-     expect(S.head('')).toEqual(O.None)
-   })
+describe('head', () => {
+  it('provides correct types', () => {
+    expectType<O.Option<string>>(S.head('text'))
+  })
 
-   it('Returns Some(value) where value is the first character of the string', () => {
-     expect(S.head('random-text')).toEqual(O.Some('r'))
-   })
+  it('returns None if the given string is empty.', () => {
+    expect(S.head('')).toEqual(O.None)
+  })
 
-   it('*', () => {
+  it('Returns Some(value) where value is the first character of the string', () => {
     expect(S.head('random-text')).toEqual(O.Some('r'))
   })
- })
+
+  it('*', () => {
+    const { Some } = O
+    expect(S.head('random-text')).toEqual(Some('r'))
+  })
+})
--- __tests__/String/last.test.ts
+++ __tests__/String/last.test.ts
@@ -1,15 +1,22 @@
+import { expectType } from 'ts-expect'
+
 import { S, O } from '../..'
 
 describe('last', () => {
+  it('provides correct types', () => {
+    expectType<O.Option<string>>(S.last('text'))
+  })
+
   it('returns None if the given string is empty.', () => {
     expect(S.last('')).toEqual(O.None)
   })
 
   it('returns Some(value) where value is the last character of the string', () => {
-    expect(S.last('random-text')).toEqual('t')
+    expect(S.last('random-text')).toEqual(O.Some('t'))
   })
 
   it('*', () => {
-    expect(S.last('random-text')).toEqual('t')
+    const { Some } = O
+    expect(S.last('random-text')).toEqual(Some('t'))
   })
 })
