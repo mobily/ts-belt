b1f16e0eb0c5e6503d9039396498d1ee1e4735fd
Marcin Dziewulski <hello@mobily.pl>
2021-12-08 23:03:45 +0100

    Update tests

--- __tests__/String/trimStart.test.ts
+++ __tests__/String/trimStart.test.ts
@@ -1,17 +1,17 @@
 import { expectType } from 'ts-expect'
 
- import { S } from '../..'
+import { S } from '../..'
 
- describe('trimStart', () => {
-   it('provides correct types', () => {
-     expectType<string>(S.trim('text'))
-   })
+describe('trimStart', () => {
+  it('provides correct types', () => {
+    expectType<string>(S.trim('text'))
+  })
 
-   it('returns a new string with leading whitespace removed from string', () => {
-     expect(S.trimStart('  text  ')).toEqual('text  ')
-   })
+  it('returns a new string with leading whitespace removed from string', () => {
+    expect(S.trimStart('  text  ')).toEqual('text  ')
+  })
 
-   it('*', () => {
-     expect(S.trimStart('  text  ')).toEqual('text  ')
-   })
- })
+  it('*', () => {
+    expect(S.trimStart('  text  ')).toEqual('text  ')
+  })
+})
