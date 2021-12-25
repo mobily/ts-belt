05db05a49deebf463953bf82a2ac53aabbff1330
Marcin Dziewulski <hello@mobily.pl>
2021-12-25 23:41:50 +0100

    Update pipe signature

--- src/pipe.ts
+++ src/pipe.ts
@@ -1,79 +1,79 @@
-export declare type UnaryFunction<T, R> = (value: T) => R
+import { UnaryFunction } from './types'
 
-export declare function pipe<T, R>(value: T, result: UnaryFunction<T, R>): R
+export declare function pipe<A, B>(value: A, fn1: UnaryFunction<A, B>): B
 
-export declare function pipe<T, A, R>(
-  value: T,
-  op1: UnaryFunction<T, A>,
-  result: UnaryFunction<A, R>,
-): R
+export declare function pipe<A, B, C>(
+  value: A,
+  fn1: UnaryFunction<A, B>,
+  fn2: UnaryFunction<B, C>,
+): C
 
-export declare function pipe<T, A, B, R>(
-  value: T,
-  op1: UnaryFunction<T, A>,
-  op2: UnaryFunction<A, B>,
-  result: UnaryFunction<B, R>,
-): R
+export declare function pipe<A, B, C, D>(
+  value: A,
+  fn1: UnaryFunction<A, B>,
+  fn2: UnaryFunction<B, C>,
+  fn3: UnaryFunction<C, D>,
+): D
 
-export declare function pipe<T, A, B, C, R>(
-  value: T,
-  op1: UnaryFunction<T, A>,
-  op2: UnaryFunction<A, B>,
-  op3: UnaryFunction<B, C>,
-  result: UnaryFunction<C, R>,
-): R
+export declare function pipe<A, B, C, D, E>(
+  value: A,
+  fn1: UnaryFunction<A, B>,
+  fn2: UnaryFunction<B, C>,
+  fn3: UnaryFunction<C, D>,
+  fn4: UnaryFunction<D, E>,
+): E
 
-export declare function pipe<T, A, B, C, D, R>(
-  value: T,
-  op1: UnaryFunction<T, A>,
-  op2: UnaryFunction<A, B>,
-  op3: UnaryFunction<B, C>,
-  op4: UnaryFunction<C, D>,
-  result: UnaryFunction<D, R>,
-): R
+export declare function pipe<A, B, C, D, E, F>(
+  value: A,
+  fn1: UnaryFunction<A, B>,
+  fn2: UnaryFunction<B, C>,
+  fn3: UnaryFunction<C, D>,
+  fn4: UnaryFunction<D, E>,
+  fn5: UnaryFunction<E, F>,
+): F
 
-export declare function pipe<T, A, B, C, D, E, R>(
-  value: T,
-  op1: UnaryFunction<T, A>,
-  op2: UnaryFunction<A, B>,
-  op3: UnaryFunction<B, C>,
-  op4: UnaryFunction<C, D>,
-  op5: UnaryFunction<D, E>,
-  result: UnaryFunction<E, R>,
-): R
+export declare function pipe<A, B, C, D, E, F, G>(
+  value: A,
+  fn1: UnaryFunction<A, B>,
+  fn2: UnaryFunction<B, C>,
+  fn3: UnaryFunction<C, D>,
+  fn4: UnaryFunction<D, E>,
+  fn5: UnaryFunction<E, F>,
+  fn6: UnaryFunction<F, G>,
+): G
 
-export declare function pipe<T, A, B, C, D, E, F, R>(
-  value: T,
-  op1: UnaryFunction<T, A>,
-  op2: UnaryFunction<A, B>,
-  op3: UnaryFunction<B, C>,
-  op4: UnaryFunction<C, D>,
-  op5: UnaryFunction<D, E>,
-  op6: UnaryFunction<E, F>,
-  result: UnaryFunction<F, R>,
-): R
+export declare function pipe<A, B, C, D, E, F, G, H>(
+  value: A,
+  fn1: UnaryFunction<A, B>,
+  fn2: UnaryFunction<B, C>,
+  fn3: UnaryFunction<C, D>,
+  fn4: UnaryFunction<D, E>,
+  fn5: UnaryFunction<E, F>,
+  fn6: UnaryFunction<F, G>,
+  fn7: UnaryFunction<G, H>,
+): H
 
-export declare function pipe<T, A, B, C, D, E, F, G, R>(
-  value: T,
-  op1: UnaryFunction<T, A>,
-  op2: UnaryFunction<A, B>,
-  op3: UnaryFunction<B, C>,
-  op4: UnaryFunction<C, D>,
-  op5: UnaryFunction<D, E>,
-  op6: UnaryFunction<E, F>,
-  op7: UnaryFunction<F, G>,
-  result: UnaryFunction<G, R>,
-): R
+export declare function pipe<A, B, C, D, E, F, G, H, I>(
+  value: A,
+  fn1: UnaryFunction<A, B>,
+  fn2: UnaryFunction<B, C>,
+  fn3: UnaryFunction<C, D>,
+  fn4: UnaryFunction<D, E>,
+  fn5: UnaryFunction<E, F>,
+  fn6: UnaryFunction<F, G>,
+  fn7: UnaryFunction<G, H>,
+  fn8: UnaryFunction<H, I>,
+): I
 
-export declare function pipe<T, A, B, C, D, E, F, G, H, R>(
-  value: T,
-  op1: UnaryFunction<T, A>,
-  op2: UnaryFunction<A, B>,
-  op3: UnaryFunction<B, C>,
-  op4: UnaryFunction<C, D>,
-  op5: UnaryFunction<D, E>,
-  op6: UnaryFunction<E, F>,
-  op7: UnaryFunction<F, G>,
-  op8: UnaryFunction<G, H>,
-  result: UnaryFunction<H, R>,
-): R
+export declare function pipe<A, B, C, D, E, F, G, H, I, J>(
+  value: A,
+  fn1: UnaryFunction<A, B>,
+  fn2: UnaryFunction<B, C>,
+  fn3: UnaryFunction<C, D>,
+  fn4: UnaryFunction<D, E>,
+  fn5: UnaryFunction<E, F>,
+  fn6: UnaryFunction<F, G>,
+  fn7: UnaryFunction<G, H>,
+  fn8: UnaryFunction<H, I>,
+  fn9: UnaryFunction<I, J>,
+): J
