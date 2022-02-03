8123b756d30dde2f6eacf413dd61e922a84a1abb
Marcin Dziewulski <hello@mobily.pl>
2022-02-03 12:08:19 +0100

    Update comment

--- tools/typescript-codemods/rename-generics.ts
+++ tools/typescript-codemods/rename-generics.ts
@@ -3,7 +3,7 @@ import { API } from 'jscodeshift'
 const transform = (source: string, j: API['jscodeshift']): string => {
   const generics = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
 
-  // T1, T2, T3 to A, B, C and so on
+  // rename T1, T2, T3… to A, B, C…
   return j(source)
     .find(j.Identifier)
     .filter(p => {
